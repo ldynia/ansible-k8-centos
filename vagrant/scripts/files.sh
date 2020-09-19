29017962b6687c28f5965184ca86f1a52938e036
ldynia <l_dynowski@yahoo.com>
2020-09-19 13:56:27 +0200

    renaming hosts updating kubeadm

--- vagrant/scripts/files.sh
+++ vagrant/scripts/files.sh
@@ -3,5 +3,3 @@
 echo "Coping static files"
 cp config/etc/hosts /etc/hosts
 cp config/etc/resolv.conf /etc/resolv.conf
-#cp config/k8s.conf /etc/sysctl.d/k8s.conf
-#cp config/kubernetes.repo /etc/yum.repos.d/kubernetes.repo
